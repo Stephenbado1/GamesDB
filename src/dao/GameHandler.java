0d79cf3dbba629addef701411917305c76f74775
Stephenbado1 <118568117+Stephenbado1@users.noreply.github.com>
2022-11-23 16:09:18 -0600

    GameHandler update

--- src/dao/GameHandler.java
+++ src/dao/GameHandler.java
@@ -5,8 +5,14 @@
  */
 package dao;
 
-import java.util.Date;
+import bo.Game;
+import java.util.ArrayList;
+import java.util.List;
 import utils.SQLUtil;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -24,5 +30,26 @@ public class GameHandler {
         String cmdTemplate = "insert into Games(gameName, pubName, releaseDate, score) values('%s', '%s', '%d', '%d')";
         String cmd = String.format(cmdTemplate, gameName, pubName, releaseDate, score);
         return sqlUtil.executeUpdate(cmd);
+    }
+    public List<Game> loadGames(String keyword){
+        List<Game> games = new ArrayList<>();
+        String cmdTemplate = "Select gID, gameName, pubName, releaseDate, score from Games where gameName like '%s'";
+        String cmd = String.format(cmdTemplate, "%" + keyword + "%");
+        ResultSet rs = sqlUtil.executeQuery(cmd);
+        try {
+            while (rs.next()){
+                //Get info
+                int gId = rs.getInt("gID");
+                String gameName = rs.getString("gameName");
+                String pubName = rs.getString("pubName");
+                int releaseDate = rs.getInt("releaseDate");
+                int score = rs.getInt("score");
+                Game game = new Game(gameName, pubName, releaseDate, score);
+                games.add(game);
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(GameHandler.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return games;
     }
 }
