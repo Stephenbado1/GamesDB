93336379d086d4ee275bc6692d9d018c59b9142b
William Pearce <billpearce25@gmail.com>
2022-11-24 21:35:44 -0600

    add Input Sanitizer

---
+++ src/utils/InputSanitizer.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package utils;
+
+/**
+ *
+ * @author bill
+ */
+public class InputSanitizer {
+    public static String sanitize(String input) {
+        String sanitized = input.replace(" ", "").replace("\\", "").replace("'", "").replace("\"", "");
+        return sanitized;
+    }
+}
